c3f28257a9a6859e253892ae918779534d597a54
zhangchengxiao <1284952151@qq.com>
2024-07-30 17:12:44 +0800

    create Test Git

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<int, int> myMap;
+    myMap.insert(std::map<int, int>::value_type(1, 2));
+    myMap.insert(std::map<int, int>::value_type(2, 4));
+    myMap.insert(std::map<int, int>::value_type(3, 6));
+    myMap.insert(std::map<int, int>::value_type(4, 8));
+    myMap.insert(std::map<int, int>::value_type(5, 10));
+    map<int, int>myMap2; 
+    myMap2.insert(std::map<int, int>::value_type(5, 12));
+    //copy(myMap.begin(), myMap.end(), inserter(myMap2, myMap2.begin()));
+    myMap2 = myMap;
+    cout << myMap2.size() << endl;
+    cout << myMap2.find(5)->second << endl;
+}
